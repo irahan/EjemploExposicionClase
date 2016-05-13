c568f38265e370ae66f0f88f687271a757ad4ef5
Irahan <irahan@gmail.com>
2016-05-13 13:19:52 -0500

    Create PlayeraPersonalizada.java

---
+++ PlayeraPersonalizada.java
@@ -0,0 +1,18 @@
+public class PlayeraPersonalizada extends Ropa{
+    
+    private String twitter;
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+    
+    public PlayeraPersonalizada(int id, double precio, String talla, String color,String twitter) {
+        super(id, precio, talla, color);
+        this.twitter=twitter;
+    }
+    
+}
